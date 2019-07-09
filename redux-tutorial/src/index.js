cd0e1a43778c2f1394a871dac5253439b1cb8f25
Eddy <edward.gonzalez9708@gmail.com>
2019-07-09 15:39:22 -0400

    Completed App component and Redux store tutorial section.

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
 
-import App from './App'
-import index from './js/index'
+import store from './js/store/index'
+import App from './js/components/App.jsx'
+
+// import index from './js/index'
 
 import './index.css'
 
-ReactDOM.render(<App />, 
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
   document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
